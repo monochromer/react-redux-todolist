bbb7e98e03fd6dd24c60c51eb3377e0672bb09c6
monochromer <monochromer@mail.ru>
2017-03-10 17:49:35 +0500

    Анимация списка

--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -1,14 +1,67 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import FlipMove from 'react-flip-move';
+import Ps from 'perfect-scrollbar';
+import 'perfect-scrollbar/dist/css/perfect-scrollbar.css';
 
 import './ToDoList.css';
 import Todo from 'components/Todo';
 
-const ToDoListItem = (props) => {
+/*class AnimateableList extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    componentWillReceiveProps() {
+        this.props.children.forEach(child => {
+            const ref = this.refs[child.key];
+            const domNode = ReactDOM.findDOMNode( ref );
+            const boundingBox = domNode.getBoundingClientRect();
+            this.setState({
+                [child.key]: boundingBox
+            });
+        });
+    }
+
+    componentDidUpdate(previousProps) {
+        previousProps.children.forEach( child => {
+            let domNode = ReactDOM.findDOMNode( this.refs[child.key] );
+
+            const newBox = domNode.getBoundingClientRect();
+            const oldBox = this.state[key];
+            
+            const deltaX = oldBox.left - newBox.left; 
+            const deltaY = oldBox.top  - newBox.top;
+
+            requestAnimationFrame( () => {
+                domNode.style.transform  = `translate(${deltaX}px, ${deltaY}px)`;
+                domNode.style.transition = 'transform 0s';
+                domNode.style.transition = '';
+                
+                requestAnimationFrame( () => {
+                    domNode.style.transform  = '';
+                    domNode.style.transition = 'transform 500ms';
+                });
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div className='ToDoList'>
+                {this.props.children}
+            </div>
+        );
+    }
+}*/
+
+/*const ToDoListItem = (props) => {
     const { todo } = props;
     const onDelete = () => props.onDelete(todo.id);
     const onChange = () => props.onChange(todo.id);
     return (
-        <li className='ToDoList-Item'>
+        <div className='ToDoList-Item'>
             <Todo
                 completed={todo.completed}
                 status={todo.status}
@@ -17,13 +70,50 @@ const ToDoListItem = (props) => {
             >
                 {todo.text}
             </Todo>
-        </li>
+        </div>
     )
+};*/
+
+class ToDoListItem extends Component {
+    constructor(props, context) {
+        super(props, context)
+    }
+
+    render() {
+        const props = this.props;
+        const { todo } = props;
+        const onDelete = () => props.onDelete(todo.id);
+        const onChange = () => props.onChange(todo.id);
+        return (
+            <div className='ToDoList-Item'>
+                <Todo
+                    completed={todo.completed}
+                    status={todo.status}
+                    onDelete={onDelete}
+                    onChange={onChange}
+                >
+                    {todo.text}
+                </Todo>
+            </div>
+        )
+    }
 };
 
 class ToDoList extends Component {
     constructor(props, context) {
         super(props, context)
+    }
+    
+    componentDidMount() {
+        Ps.initialize(this.list);
+    }
+
+    componentDidUpdate() {
+        setTimeout(() => { Ps.update(this.list) }, 400);
+    }
+
+    componentWillUnmount() {
+        Ps.destroy(this.list);
     }
 
     render() {
@@ -42,9 +132,16 @@ class ToDoList extends Component {
         });
 
         return (
-            <ul className='ToDoList'>
-                {tasks}
-            </ul>
+            <div ref={node => this.list = node} className='ToDoList'>
+                <FlipMove
+                    duration={400}
+                    easing='ease-out'
+                    enterAnimation="fade"
+                    leaveAnimation="fade"
+                >
+                    {tasks}
+                </FlipMove>
+            </div>
         )
     }
 };
