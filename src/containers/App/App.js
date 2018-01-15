6090911c8c571e8c09eeba55f89ccd51ced6075f
monochromer <monochromer@mail.ru>
2018-01-15 18:13:30 +0500

    move app component to stateless

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -6,17 +6,14 @@ import AddFormContainer from 'containers/AddFormContainer';
 import FilterContainer from 'containers/FilterContainer';
 import ToDoListContainer from 'containers/ToDoListContainer';
 
-class App extends Component {
-
-    render() {
-        return (
-            <div className="ToDoApp">
-                <AddFormContainer />
-                <FilterContainer/>
-                <ToDoListContainer/>
-            </div>
-        )
-    }
-}
+const App = () => {
+    return (
+        <div className="ToDoApp">
+            <AddFormContainer />
+            <FilterContainer/>
+            <ToDoListContainer/>
+        </div>
+    )
+};
 
 export default App;
