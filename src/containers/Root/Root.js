654000412f8923765c0f90fb462596a2e2091e09
monochromer <monochromer@mail.ru>
2018-01-22 11:29:13 +0500

    change index for root component

---
+++ src/containers/Root/Root.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import 'normalize.css/normalize.css';
+import App from 'components/App';
+
+const Root = ({ store }) => {
+    return (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
+}
+
+export default Root;
--- src/containers/Root/index.js
+++ src/containers/Root/index.js
@@ -1,15 +1,1 @@
-import React from 'react';
-import {Provider} from 'react-redux';
-
-import 'normalize.css/normalize.css';
-import App from 'components/App';
-
-const Root = ({ store }) => {
-    return (
-        <Provider store={store}>
-            <App />
-        </Provider>
-    );
-}
-
-export default Root;
+export { default } from './Root';
