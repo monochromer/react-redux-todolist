fffe526f1413061396e26a64affd6b2ced559165
monochromer <monochromer@mail.ru>
2018-01-22 14:59:52 +0500

    restore .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -100,27 +100,7 @@ var config = {
             {
                 test: /\.js$/,
                 use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        "presets": [
-                            "es2015",
-                            "stage-0",
-                            "react"
-                        ],
-                        "plugins": [
-                            "transform-runtime",
-                            "transform-object-rest-spread",
-                            "transform-es2015-modules-commonjs"
-                        ],
-                        "env": {
-                            "production": {
-                                "plugins": [
-                                    "transform-react-constant-elements",
-                                    "transform-react-inline-elements"
-                                ]
-                            }
-                        }
-                    }
+                    loader: 'babel-loader'
                 }],
                 exclude: [/node_modules/, /public/]
             },
