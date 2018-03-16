df8cf3f40c9c1c2b83801114e5b38653cbe3cbaa
monochromer <monochromer@mail.ru>
2018-03-16 14:22:21 +0500

    move to webpack 4

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const compression = require('compression');
 const helmet = require('helmet');
 
 const PORT = process.env.PORT || 9000;
+const ASSETS_PATH = path.join(__dirname, 'public');
+
 const app = express();
 
 app.use(helmet());
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(ASSETS_PATH));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
